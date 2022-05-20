5117af5056d4335a5e0d58238afea3cb1960d4c0
Arjun <arjunparidwal1998@gmail.com>
2022-05-20 17:51:57 +0530

    title

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html>
        <Head>
         <meta charset="utf-8" />
-        <title>{`Clean Hedge`}</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta content="" name="description" />
         <meta content="" name="keywords" />
