ad2cfeb825f9fea3cb2eb07cf48bb515b2f807cc
Arjun <arjunparidwal1998@gmail.com>
2022-05-27 00:37:17 +0530

    homepagechanges

--- pages/404.js
+++ pages/404.js
@@ -6,10 +6,10 @@ export default function Error() {
     <div id="main">
         <div className="fof">
             <div id="logo">
-				<a href="/"> <img src={`images/background/Clean-Hedge-logo-black.png`} alt="" width="auto" height="45" /> </a>
+				<a href="http://clean-hedge.com/"> <img src={`images/background/Clean-Hedge-logo-black.png`} alt="" width="auto" height="45" /> </a>
 			</div>
           <center><h1 style={{color:'black'}}>404 Error<span className="id-color"><br/>The Requested Url Was Not Found on This Server</span></h1>
-                    <div className="spacer-50"></div><a className="btn-custom scroll-to wow fadeInUp" href="/">Go Back</a>
+                    <div className="spacer-50"></div><a className="btn-custom scroll-to wow fadeInUp" href="http://clean-hedge.com/">Go Back</a>
             </center>
     	</div>
     </div>
