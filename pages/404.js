a84c3957e14433b9fd5bb37dd8d5e3d603a9dde5
Arjun <arjunparidwal1998@gmail.com>
2022-05-27 01:02:06 +0530

    imageschangess

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,10 @@
-import React from 'react'
-
 export default function Error() {
   return (
     <>
     <div id="main">
         <div className="fof">
             <div id="logo">
-				<img src={`images/background/Clean-Hedge-logo-black.png`} alt="" width="auto" height="45"></img>
+            <a href=""> <img src={`images/background/Clean-Hedge-logo-black.png`} alt="me" width="auto" height="45" /> </a>
 			</div>
           <center><h1 style={{color:'black'}}>404 Error<span className="id-color"><br/>The Requested Url Was Not Found on This Server</span></h1>
                     <div className="spacer-50"></div><a className="btn-custom scroll-to wow fadeInUp" href="/index">Go Back</a>
