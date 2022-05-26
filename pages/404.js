477ac3faffdfbc0a9150ab8f104ec3fd043935dc
Arjun <arjunparidwal1998@gmail.com>
2022-05-27 00:48:10 +0530

    homepagechanges

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Error() {
@@ -6,7 +7,7 @@ export default function Error() {
     <div id="main">
         <div className="fof">
             <div id="logo">
-				<a href="/index" /> <img src={`images/background/Clean-Hedge-logo-black.png`} alt="" width="auto" height="45"/>
+				<Link href="/index" /> <img src={`images/background/Clean-Hedge-logo-black.png`} alt="" width="auto" height="45"/>
 			</div>
           <center><h1 style={{color:'black'}}>404 Error<span className="id-color"><br/>The Requested Url Was Not Found on This Server</span></h1>
                     <div className="spacer-50"></div><a className="btn-custom scroll-to wow fadeInUp" href="/index">Go Back</a>
