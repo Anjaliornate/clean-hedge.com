0582ba92f18b56a9492c32d9d2110d0d51f1c7ad
Arjun <arjunparidwal1998@gmail.com>
2022-05-27 12:12:04 +0530

    indexremoves

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,35 +1,35 @@
 
-export default function blog({data}) {
-    console.log(data)
-    return (
-    <>
-    <div className="col-m-12">
-    {data.map((post,index)=>{
+// export default function blog({data}) {
+//     console.log(data)
+//     return (
+//     <>
+//     <div className="col-m-12">
+//     {data.map((post,index)=>{
     
-    return (
-        <div>
-        <div className="container">
-	<div className="row">
-		<div className="col-md-9">
-			<div className="card"><img src={post[ '_embedded'][ 'wp:featuredmedia'][0][ 'source_url']} className="card-img-top" style={{height: '500px'}}/>
-				<div className="card-body">
-					<h5 className="card-title">{post['title']['rendered']}</h5>
-					<div className="card-text" dangerouslySetInnerHTML={{__html:post[ 'content'][ 'rendered']}}></div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-        </div>);
-    })}
-    </div>
-    </>
-    )
-};
+//     return (
+//         <div>
+//         <div className="container">
+// 	<div className="row">
+// 		<div className="col-md-9">
+// 			<div className="card"><img src={post[ '_embedded'][ 'wp:featuredmedia'][0][ 'source_url']} className="card-img-top" style={{height: '500px'}}/>
+// 				<div className="card-body">
+// 					<h5 className="card-title">{post['title']['rendered']}</h5>
+// 					<div className="card-text" dangerouslySetInnerHTML={{__html:post[ 'content'][ 'rendered']}}></div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	</div>
+// </div>
+//         </div>);
+//     })}
+//     </div>
+//     </>
+//     )
+// };
 
-export async function getServerSideProps(context) {
-    const {id} = context.params;
-    const res = await fetch(`https://blog.ted.com/wp-json/wp/v2/posts?_embed&slug=${id}`);
-    const data = await res.json();
-    return {props: {data}}
-}
+// export async function getServerSideProps(context) {
+//     const {id} = context.params;
+//     const res = await fetch(`https://blog.ted.com/wp-json/wp/v2/posts?_embed&slug=${id}`);
+//     const data = await res.json();
+//     return {props: {data}}
+// }
