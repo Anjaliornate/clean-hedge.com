52145a95cb5b36dad6bf4efad2fca5b70ed9ad53
Arjun <arjunparidwal1998@gmail.com>
2022-06-22 11:09:15 +0530

    url

--- pages/blogs.js
+++ pages/blogs.js
@@ -61,11 +61,10 @@
 }
 
   export async function getServerSideProps(){
-      const getPosts = await fetch('http://65.0.181.105/wp-json/wp/v2/posts?_embed');
+      const getPosts = await fetch('https://kinsta.com/wp-json/wp/v2/posts?_embed&per_page=100'); 
       const data =await getPosts.json();
       return {props: {data}}
       
   }
-
 
   
