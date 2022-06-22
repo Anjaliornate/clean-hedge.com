dade114fd7e9e2bd92a1aaece6d1c4669a62186c
Arjun <arjunparidwal1998@gmail.com>
2022-06-22 11:11:43 +0530

    url

--- pages/blogs.js
+++ pages/blogs.js
@@ -61,7 +61,7 @@
 }
 
   export async function getServerSideProps(){
-      const getPosts = await fetch('https://kinsta.com/wp-json/wp/v2/posts?_embed&per_page=100'); 
+      const getPosts = await fetch('https://bilberrry.com/wp-json/wp/v2/posts?_embed&per_page=100'); 
       const data =await getPosts.json();
       return {props: {data}}
       
